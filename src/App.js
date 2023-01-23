187dc7e207abfa4af683ece8d0013263d011516f
Cata Villalonga <cata.villalonga21@gmail.com>
2023-01-23 16:56:41 +0100

    added collaborations section"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from'./Components/Header'
 import Presentation from './Components/Presentation';
 import TechStack from'./Components/TechStack'
 import Projects from'./Components/Projects'
+import Collaborations from'./Components/Collaborations'
 import './Styles/reset.css';
 import './Styles/App.css';
 
@@ -12,6 +13,7 @@ function App() {
     <Presentation/>
     <TechStack />
     <Projects />
+    < Collaborations />
     </>
   );
 }
--- src/Components/Collaborations.jsx
+++ src/Components/Collaborations.jsx
@@ -1,11 +1,19 @@
-import '../Styles/Footer.css';
+import saltLogo from '../logos/salt.png'
+import metrieveLogo from '../logos/metrieve.png'
+import smartCodingLogo from '../logos/smartCoding.gif'
+import '../Styles/Collaborations.css';
 
 const Collaborations =() => {
   return (
-    <section id='collaborations'>
-      <img src=''/>
-      <img src=''/>
-      <img src=''/>
+    <section id='collaborations' className='collaborations-section'>
+      <div className='collaborations-section-title'>
+        <h1>Collaborations</h1>
+      </div>
+      <div className='collaborations-section-imgs'>
+      <img id='smartcoding'src={smartCodingLogo}/>
+      <img src={metrieveLogo}/>
+      <img src={saltLogo}/>
+      </div>
     </section>
   )
 }
